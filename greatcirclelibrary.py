0177383b1c18abbabfb583815cee64a2f9892c45
Nathaniel Selub <nathanielselub@cs.uchicago.edu>
2021-02-18 18:25:33 +0100

    Revert "Fixed FFT Bug"
    This reverts commit 542d128b9de919b9414b38d7c097364a478c8135.

--- greatcirclelibrary.py
+++ greatcirclelibrary.py
@@ -355,14 +355,13 @@ def gc_power_spectra(gc_pix, alms, nside=NSIDE):
         The power spectra of great circles from multiple maps.
 
     """
-    spc = np.empty([alms.shape[0], gc_pix.shape[0], 1 + gc_pix.shape[1] // 2])
+    spec = np.empty([alms.shape[0], gc_pix.shape[0], 1 + gc_pix.shape[1] // 2])
     for i in prange(alms.shape[0]):
-        spc[i] = np.abs(np.fft.rfft(np.take(hp.sphtfunc.alm2map(alms[i],
-                                                                nside,
-                                                                verbose=False),
-                                            gc_pix), axis=1)) ** 2
+        spec[i] = np.fft.rfft(np.take(hp.sphtfunc.alm2map(alms[i], nside,
+                                                          verbose=False),
+                                      gc_pix), axis=1)
 
-    return spc
+    return np.abs(spec) ** 2
 
 
 @guvectorize([(int64[:], float64[:], float64[:])], '(m),(n)->()',
